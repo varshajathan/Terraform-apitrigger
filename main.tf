1855ea9e17587aeb49c4f465ec8daaca05c5f824
varshajathan <48399281+varshajathan@users.noreply.github.com>
2025-04-21 13:37:29 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,42 @@
+# main.tf
+
+terraform {
+  required_version = ">= 1.0"
+  backend "remote" {
+    organization = "your-org-name"
+
+    workspaces {
+      name = "your-workspace-name"
+    }
+  }
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "google" {
+  project     = var.project_id
+  region      = var.region
+  credentials = file(var.credentials_file)
+}
+
+resource "google_compute_instance" "default" {
+  name         = "terraform-instance"
+  machine_type = "e2-micro"
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network       = "default"
+    access_config {}
+  }
+}
